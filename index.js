78aeeb536d16a2db5c6da2d681ed6e26c2a60407
SharonSmith <mssharonsmiths98@gmail.com>
2024-01-24 17:16:59 -0500

    added new ejs files

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 const express = require('express')    /*holding place*/
-const app = express()
 const path = require('path')
+
+const app = new express()
+const ejs =require('ejs')
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
 
 app.listen(3000,()=>{
     console.log("App listening on port 3000")
 })
-
 app.get('/',(req,res)=>{
-   res.sendFile(path.resolve(__dirname, 'index.html'))
-    })
+    res.sendFile(path.resolve(__dirname,'pages/index.html'))
+})
 
+app.get('/contact',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+    res.render('contact');
+})
+app.get('/post',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/post.html'))
+    res.render('post');
+})
 
 app.get('/about',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'about.html'))
-   })
-
-   app.get('/contact',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'contact.html'))
-   })
-
-   app.get('/notfound',(req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'notfound.html'))
-   })
+    res.sendFile(path.resolve(__dirname,'pages/about.html'))
+    res.render('about');
+})
