0d15d6e9fb01fdfe9114cf551f9cd580748f6332
SharonSmith <mssharonsmiths98@gmail.com>
2024-01-29 17:12:57 -0500

    controllers

--- index.js
+++ index.js
@@ -1,53 +1,64 @@
 const express = require('express')
-const path = require('path')
-
 const app = express()
 
 const ejs = require('ejs')
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 
 const mongoose = require('mongoose')
-const BlogPost = require('./models/BlogPost.js')
-mongoose.connect('mongodb://localhost/my_database',{useNewUrlParser:true,useUnifiedTopology:true})
+
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.listen(3000,()=>{
-    console.log("App listening on port 3000")
-})
+const validateMiddleWare = (req, res, next) => {
+    if (req.files === null || req.body.title == null) {
+        return res.redirect('/posts/new')
+    }
+    next()
+}
+app.use('posts/store', validateMiddleWare)
 
-app.get('/',async(req,res)=>{
+const homeController = require('./controllers/home')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
+
+
+    .
+
+
+    app.listen(3000, () => {
+        console.log("App listening on port 3000")
+    })
+
+app.get('/', async (req, res) => {
     const blogposts = await BlogPost.find({})
-    res.render('index',{
+    res.render('index', {
         blogposts
     })
 })
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.render('about')
 })
 
-app.get('/contact',(req,res)=>{
+app.get('/contact', (req, res) => {
     res.render('contact')
 })
+const newPostController = require('./controllers/newPost')
+app.get('/post/new', newPostController)
 
-app.get('/post/new',(req,res)=>{
-    res.render('create')
+app.post('/posts/store', async (req, res) => {
+    // model creates a new doc with browser data
+    await BlogPost.create(req.body, (error, blogpost) => {
+        res.redirect('/')
+    })
 })
 
-app.post('/posts/store',async(req,res)=>{
+app.get('/post/:id', async (req, res) => {
     const blogpost = await BlogPost.findById(req.params.id)
-    res.render('post',{blogpost})
-    // model creates a new doc with browser data
-    //await BlogPost.create(req.body,(error,blogpost)=>{res.redirect('/')//
-    })
-
-
-app.get('/post/:id',async(req,res)=>{
-    const blogpost = await BlogPost.findById(req.params.id)
-    res.render('post',{
+    res.render('post', {
         blogpost
     })
 }) 
